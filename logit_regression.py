c4e5b06111710c779fa42743729256b6e86553eb
Sanandthe11 <sanandclt99@gmail.com>
2025-08-20 13:37:52 +0530

    Update logit_regression.py
    Addition for title

--- logit_regression.py
+++ logit_regression.py
@@ -7,10 +7,24 @@ import warnings
 warnings.filterwarnings("ignore")
 import os
 from sklearn import preprocessing
-
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_recall_curve
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn.neural_network import MLPClassifier
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from keras.layers import Dropout
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import BatchNormalization
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import auc
 
 #Load the data
 df =pd.read_csv(r'C:\Users\SHEEJA\Downloads\creditcard_2023.csv\creditcard_2023_1.csv')
@@ -23,38 +37,19 @@ df['std_Amount'] = scaler.fit_transform(df['Amount'].values.reshape (-1,1))
 df = df.drop("Amount", axis=1)
 
 #plot the data to find the distribution of type of transactions
-#sns.countplot(x='Class',data=df)
-#plt.show()
+sns.countplot(x='Class',data=df)
+plt.show()
 
+# separate the Dependent and Independent
 cols = df.columns.tolist()
 cols = [c for c in cols if c not in ["Class"]]
 target = "Class"
 X = df[cols]
 Y = df[target]
 
-
+# Create Train test split 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
-
-#importing packages for modeling
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from xgboost import XGBClassifier
-from sklearn.neural_network import MLPClassifier
-
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-from keras.layers import Dropout
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import BatchNormalization
-
-
-from sklearn.metrics import roc_curve
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import auc
 
 # 1) Logistic Regression
 model1 = LogisticRegression(random_state=2)
@@ -71,11 +66,11 @@ print("F1 Score Logit:",metrics.f1_score(y_test, y_pred_logit))
 matrix_logit = confusion_matrix(y_test, y_pred_logit)
 cm_logit = pd.DataFrame(matrix_logit, index=['Actual', 'fraud'], columns=['Actual', 'fraud'])
 
+# Create Graph
 sns.heatmap(cm_logit, annot=True, cbar=None, cmap="Blues", fmt = 'g')
 plt.title("Confusion Matrix Of Logit"), plt.tight_layout()
 plt.ylabel("Actual Class"), plt.xlabel("Predicted Class")
 plt.show()
-
 y_pred_logit_proba = model1.predict_proba(X_test)[::,1]
 fpr_logit, tpr_logit, _ = metrics.roc_curve(y_test,  y_pred_logit_proba)
 auc_logit = metrics.roc_auc_score(y_test, y_pred_logit_proba)
@@ -90,6 +85,7 @@ plt.title('Logistic Regression ROC curve')
 plt.legend(loc=4)
 plt.show()
 
+#Recall curve
 logit_precision, logit_recall, _ = precision_recall_curve(y_test, y_pred_logit_proba)
 no_skill = len(y_test[y_test==1]) / len(y_test)
 plt.plot([0, 1], [no_skill, no_skill], linestyle='--', color='black', label='No Skill')
